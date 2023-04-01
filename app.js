8ce97161e8433312b4dd620aca4f519fe54beed8
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-04-01 21:59:50 +0530

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+app.get('/',(req,res)=>{
+    res.send({message:"Hello world"});
+});
+
+app.listen(PORT,()=>{
+    console.log(`Running on ${PORT}`);
+})
